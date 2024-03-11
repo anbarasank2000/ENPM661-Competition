b75ee5ea9fb972262235db5de163c37db9256737
bat <anbarasank2000@gmail.com>
2024-03-10 23:29:28 -0400

    Final
    Improved the code efficiency by avoiding unwanted for loops and optimized the code.

--- dijkstra_anbarasan_kandasamy.py
+++ dijkstra_anbarasan_kandasamy.py
@@ -2,54 +2,39 @@ import heapq
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-# output video
-fourcc = cv2.VideoWriter_fourcc(*'XVID')  # You can use other codecs like 'MJPG' or 'H264'
-fps = 25.0  # Frames per second
-width, height = 640, 480  # Video resolution
-out = cv2.VideoWriter('output_video.mp4', fourcc, fps, (width, height))
+
 # map
 canvas = np.ones((500, 1200, 3))*np.inf
-# c2c_map = np.ones((500, 1200, 1))*np.inf
 cv2.rectangle(canvas, (95,0), (180,405), (0,0,255), -1)
-# cv2.rectangle(c2c_map, (95,0), (180,405), (-1), -1)
 cv2.rectangle(canvas, (270,95), (355,500), (0,0,255), -1)
-# cv2.rectangle(c2c_map, (280,105), (355,500), (-1), -1)
 hex = np.array([[515,175],[650,95],[785,175],[785,325],[650,405],[515,325]])
 hex = hex.reshape(-1,1,2)
 cv2.fillPoly(canvas,[hex], color=(0,0,255))
-# cv2.fillPoly(c2c_map,[hex], color=(-1))
 c_shape = np.array([[895,45],[1105,45],[1105,455],[895,455],[895,370],[1015,370],[1015,130],[895,130]])
 c_shape = c_shape.reshape(-1,1,2)
 cv2.fillPoly(canvas,[c_shape], color=(0,0,255))
-# cv2.fillPoly(c2c_map,[c_shape], color=(-1))
 count = 0
-# print(canvas)
-# plt.imshow(canvas)
-# plt.show()
 start = (8,8)
-end = (8,300)
-# cv2.circle(canvas, (end[0],500-end[1]), 4, (255,255,0), -1)
+end = (1194,340)
 if canvas[500-start[1], start[0]][2] !=np.inf:
     print("please enter new start value")
 if canvas[500-end[1], end[0]][2] !=np.inf:
     print("please enter new end value")
 print([500-end[1],end[0]])
 print(canvas[500-end[1],end[0]][2])
-cv2.circle(canvas, (end[0],500-end[1]), 4, (255,255,0), -1)
-# cv2.imshow("ss", canvas)
-# cv2.waitKey(0)
-# print(canvas[160,514][2]==np.inf)
+
+
 
 current_state = (start, start)
 q_open = []
 q_closed = []
+close_heap_dict = {}
+all_node = []
+back_track_node = []
 heapq.heapify(q_open)
-# heapq.heapify(q_closed)
 heapq.heappush(q_open, (0, current_state))
-# print(q_open[0][1][1])
 
 def up(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -66,15 +51,11 @@ def up(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
-                heapq.heappush(q_open, (current_cost, current_state))     
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0) 
+                heapq.heappush(q_open, (current_cost, current_state))    
                 # print("up")     
     return q_open, canvas
 def down(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -91,15 +72,11 @@ def down(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state)) 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
                 # print("down")
     return q_open, canvas
 def left(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -116,15 +93,11 @@ def left(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state)) 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
                 # print("left")
     return q_open, canvas
 def right(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -141,15 +114,11 @@ def right(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state)) 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
                 # print("right")
     return q_open, canvas
 def up_left(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -166,15 +135,11 @@ def up_left(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state))
                 # print("up_left") 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
     return q_open, canvas
 def up_right(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -191,15 +156,11 @@ def up_right(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state)) 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
                 # print("up_right")
     return q_open, canvas
 def down_left(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -216,15 +177,11 @@ def down_left(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state)) 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
                 # print("down_left")
     return q_open, canvas
 def down_right(q_closed, q_open, canvas, close_heap_dict):
-    # close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
     open_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_open)}
     current_cost = q_closed[-1][0]
     current_node = q_closed[-1][1][0]
@@ -241,15 +198,10 @@ def down_right(q_closed, q_open, canvas, close_heap_dict):
                 index = open_heap_dict[current_node]
                 if q_open[index][0]>current_cost:
                     q_open[index] = (current_cost, current_state)
-                    # q_open[index][0] = current_cost
-                    # q_open[index][1][1] = current_parrent
             else:
                 heapq.heappush(q_open, (current_cost, current_state)) 
-                # canvas[500-current_node[1],current_node[0]] = (255,0,0)
-                # print("down_right")
     return q_open, canvas
-def back_tracking(q_closed, canvas, count, out, close_heap_dict):
-    # final_closed = {pair[1][0]: index for index, pair in enumerate(q_closed)}
+def back_tracking(q_closed, canvas, count, close_heap_dict):
     if q_closed[-1][1][0] == end:
         node_state = q_closed[-1]
         node = node_state[1][0]
@@ -258,46 +210,68 @@ def back_tracking(q_closed, canvas, count, out, close_heap_dict):
             # print(node)
             count+=1
             index = close_heap_dict[node]
-            # print(node)
             node_state = q_closed[index]
             node = node_state[1][1]
-            canvas[500-node[1],node[0]] = ( 0, 255, 255 )
-            # visualize(canvas, count, out)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
+            back_track_node.append(node)
         (print("bt_finished"))
         return canvas
-def visualize(canvas, count, out, current_Node):
-    if count%100 == 0:
-        # canvas[500-current_Node[1],current_Node[0]] = (255,0,0)
-        frame = canvas
-        out.write(frame)
-        cv2.imshow('Window Name', frame)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-        # cv2.waitKey(0)
-
+def visualize(canvas, count, current_Node, back_track_node, all_node, vis_i, vis_j):
+    canvas_i = canvas.astype(np.uint8)
+    back_track_node.reverse()
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('path_visualization.mp4', fourcc, 20.0, (1200, 500))
+    for i in all_node:
+        cv2.circle(canvas_i, (i[0],500-i[1]), 0, (255,0,0))
+        vis_i+=1
+        if vis_i%1000 == 0:
+            out.write(canvas_i)
+        # out.release()
+    for j in back_track_node:
+        cv2.circle(canvas_i, (j[0],500-j[1]), 0, (255,255,0))
+        vis_j+=1
+        if vis_j%10 == 0:
+            out.write(canvas_i)
+    for i in range(50):
+        out.write(canvas_i)
+    cv2.circle(canvas, (end[0],500-end[1]), 4, (255,255,0), -1)
+    cv2.circle(canvas, (start[0],500-start[1]), 4, (255,255,0), -1)
+    out.release()
+    # cv2.imshow('canva', canvas_i)
+    # cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+dict_i = 0
+vis_i = 0
+vis_j = 0
 while True:
     count+=1
-    current_State = heapq.heappop(q_open)
-    current_Node = current_State[1][0]
+    current = heapq.heappop(q_open)
+    current_Cost = current[0]
+    current_Node = current[1][0]
     # print(current_Node)
     if len(q_open) != 0 or count == 1:
-        q_closed.append(current_State)
-        close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
-        # print(close_heap_dict)
+        q_closed.append(current)
+        close_heap_dict[current_Node] = dict_i
+        dict_i+=1
         if current_Node == end:
             print("solution found")
-            canvas = back_tracking(q_closed, canvas, count, out, close_heap_dict)
+            canvas = back_tracking(q_closed, canvas, count, close_heap_dict)
+            visualize(canvas, count, current_Node, back_track_node, all_node, vis_i, vis_j)
             break
         else:
-            up()
-            down()
-            left()
-            right()
+            q_open, _ = up(q_closed, q_open, canvas, close_heap_dict)
+            # print(q_open)
+            q_open, _ = down(q_closed, q_open, canvas, close_heap_dict)
+            q_open, _ = left(q_closed, q_open, canvas, close_heap_dict)
+            q_open, _ = right(q_closed, q_open, canvas, close_heap_dict)
+            q_open, _ = up_left(q_closed, q_open, canvas, close_heap_dict)
+            q_open, _ = up_right(q_closed, q_open, canvas, close_heap_dict)
+            q_open, _ = down_left(q_closed, q_open, canvas, close_heap_dict)
+            q_open, _ = down_right(q_closed, q_open, canvas, close_heap_dict)
+            # print(q_open)
+            all_node.append(current_Node)
     else:
         print("No solution found")
         break
-out.release()
 # Close any open windows
 cv2.destroyAllWindows()
