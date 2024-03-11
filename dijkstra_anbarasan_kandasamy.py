c42613728456d72a1cdf96f4f7c477c31776c45d
bat <anbarasank2000@gmail.com>
2024-03-10 20:27:08 -0400

    Basic Structure
    Created move functions and while loop

--- dijkstra_anbarasan_kandasamy.py
+++ dijkstra_anbarasan_kandasamy.py
@@ -22,3 +22,63 @@ c_shape = np.array([[895,45],[1105,45],[1105,455],[895,455],[895,370],[1015,370]
 c_shape = c_shape.reshape(-1,1,2)
 cv2.fillPoly(canvas,[c_shape], color=(0,0,255))
 # cv2.fillPoly(c2c_map,[c_shape], color=(-1))
+count = 0
+# print(canvas)
+# plt.imshow(canvas)
+# plt.show()
+start = (8,8)
+end = (8,300)
+# cv2.circle(canvas, (end[0],500-end[1]), 4, (255,255,0), -1)
+if canvas[500-start[1], start[0]][2] !=np.inf:
+    print("please enter new start value")
+if canvas[500-end[1], end[0]][2] !=np.inf:
+    print("please enter new end value")
+print([500-end[1],end[0]])
+print(canvas[500-end[1],end[0]][2])
+cv2.circle(canvas, (end[0],500-end[1]), 4, (255,255,0), -1)
+# cv2.imshow("ss", canvas)
+# cv2.waitKey(0)
+# print(canvas[160,514][2]==np.inf)
+
+current_state = (start, start)
+q_open = []
+q_closed = []
+heapq.heapify(q_open)
+# heapq.heapify(q_closed)
+heapq.heappush(q_open, (0, current_state))
+# print(q_open[0][1][1])
+
+def up():
+
+def down():
+
+def left():
+
+def right():
+
+def back_tracking():
+    
+while True:
+    count+=1
+    current_State = heapq.heappop(q_open)
+    current_Node = current_State[1][0]
+    # print(current_Node)
+    if len(q_open) != 0 or count == 1:
+        q_closed.append(current_State)
+        close_heap_dict = {pair[1][0]: index for index, pair in enumerate(q_closed)}
+        # print(close_heap_dict)
+        if current_Node == end:
+            print("solution found")
+            canvas = back_tracking(q_closed, canvas, count, out, close_heap_dict)
+            break
+        else:
+            up()
+            down()
+            left()
+            right()
+    else:
+        print("No solution found")
+        break
+out.release()
+# Close any open windows
+cv2.destroyAllWindows()
